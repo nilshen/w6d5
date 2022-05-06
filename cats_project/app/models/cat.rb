32e9611e491eb13665b923533c4cfa52f17f7569
App Academy Student <aa@Apps-Mac-mini-7.local>
2022-05-06 16:25:10 -0400

    Up to Unify, Debugging

--- cats_project/app/models/cat.rb
+++ cats_project/app/models/cat.rb
@@ -1,5 +1,5 @@
 class Cat < ApplicationRecord
-    validates :name, :birth_date, presence :true
+    validates :name, :birth_date, presence: :true
     validates :sex, inclusion: { in: %w(M F) }
     validates :color, inclusion: { in: %w(brunette blonde black) }
 end
