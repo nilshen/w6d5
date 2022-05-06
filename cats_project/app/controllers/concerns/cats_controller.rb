29cb3ead2c00509dedcad4f77fd4bb15dc5f38c9
App Academy Student <aa@Apps-Mac-mini-7.local>
2022-05-06 15:35:35 -0400

    Up to Unify! on Cat Project

--- cats_project/app/controllers/concerns/cats_controller.rb
+++ cats_project/app/controllers/concerns/cats_controller.rb
@@ -10,10 +10,38 @@ class CatsController < ApplicationController
         render :show
     end
 
-    
+    def new
+        @cat = Cat.new
+        render :new
+    end
 
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render @cat.errors.full_messages, status: unprocessable_entity
+        end
+    end
 
+    def edit
+        @cat = Cat.find(params[:id])
+        render :edit
+    end
 
+    def update
+        @cat = Cat.find(params[:id])
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render @cat.errors.full_messages, status: unprocessable_entity
+        end
+    end
 
+    private
+
+    def cat_params
+        require(:cat).permit(:name, :color, :sex, :description, :birth_date)
+    end
 
 end
