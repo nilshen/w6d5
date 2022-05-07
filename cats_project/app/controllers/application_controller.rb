1432d76729b141d2381a660c5d0858775f374084
nilshen <linshen88@gmail.com>
2022-05-07 12:39:31 -0400

    cats all live

--- cats_project/app/controllers/application_controller.rb
+++ cats_project/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     # 
     # protect_from_forgery with: :exception
     protect_from_forgery prepend: true
-    # skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
     # protect_from_forgery with: :null_session
     # protect_from_forgery prepend: true, with: :exception
   
